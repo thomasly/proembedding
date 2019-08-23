e3ee68bae12da47944aad8fceea5bc3024b1876b
Yang Liu <thomasliuy@gmail.com>
2019-08-23 13:26:14 -0400

    read pdb files from tar.gz file

--- src/dataprep.py
+++ src/dataprep.py
@@ -4,6 +4,7 @@ import tarfile
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 class TOUGH_C1:
 
@@ -37,14 +38,54 @@ class TOUGH_C1:
 
     def _load_tar(self, path):
         tar = tarfile.open(os.path.join(self.root, path))
-        tar.list(verbose=False)
-
+        pdb_info = dict()
+        Atom = namedtuple(
+                    "atom",
+                    "atom_id atom_name residue_id residue_name x y z"
+                )
+        for pdb in tqdm(tar.getmembers()):
+            if not pdb.isfile():
+                continue
+            if pdb.name.split("/")[1].startswith("."):
+                continue
+            name = pdb.name.split("/")[1].split(".")[0]
+            try:
+                assert(len(name)==5)
+            except AssertionError:
+                print(f"{pdb.name} is not a valid pdb file")
+                continue
+            atom_ls = list()
+            pdb_f = tar.extractfile(pdb)
+            lines = pdb_f.readlines()
+            pdb_f.close()
+            for line in lines:
+                if not line.startswith(b"ATOM"):
+                    continue
+                line = line.decode("utf-8")
+                atom_id = int(line[6:11])
+                atom_name = line[12:16].strip()
+                residue_id = int(line[22:26])
+                residue_name = line[17:20].strip()
+                x = float(line[30:38])
+                y = float(line[38:46])
+                z = float(line[46:54])
+                atom = Atom(
+                    atom_id, atom_name, residue_id, residue_name, x, y, z)
+                atom_ls.append(atom)
+            pdb_info[name] = atom_ls
+        
+        return pdb_info
 
 
 if __name__ == "__main__":
+    def print_first(d):
+        key = list(d.keys())[0]
+        print(key)
+        print(d[key][0:10])
+
     t = TOUGH_C1()
     print(f"control: {t.control_ls[0:2]}")
     print(f"heme: {t.heme_ls[0:2]}")
     print(f"nucleotide: {t.nucleotide_ls[0:2]}")
     print(f"steroid: {t.steroid_ls[0:2]}")
-    # t._load_tar("protein-control.tar.gz")
+    print_first(t._load_tar("protein-control.tar.gz"))
