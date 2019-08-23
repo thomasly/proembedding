c29f7fc596e6de5538a5dc8e64462974856c303e
Yang Liu <thomasliuy@gmail.com>
2019-08-23 17:20:22 -0400

    split TOUGH-C1 into train and test sets

--- src/dataprep.py
+++ src/dataprep.py
@@ -1,6 +1,7 @@
 import os
 from collections import namedtuple
 import tarfile
+import random
 
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ from tqdm import tqdm
 
 class TOUGH_C1:
 
-    def __init__(self):
+    def __init__(self, random_seed=0, train_test_ratio=0.9):
         # root path of the TOUGH-C1 data
         self.root = os.path.join(os.path.pardir, "data", "osfstorage-archive")
         # dict for label generation
@@ -23,7 +24,10 @@ class TOUGH_C1:
         self.heme_ls = self._load_lst("deepdrug3d_heme.lst")
         self.nucleotide_ls = self._load_lst("deepdrug3d_nucleotide.lst")
         self.steroid_ls = self._load_lst("deepdrug3d_steroid.lst")
-
+        # create training and testing sets
+        self.random_seed = random_seed
+        self.train_ls, self.test_ls = self._split_dataset(train_test_ratio)
+        
     def _load_lst(self, path):
         file_path = os.path.join(self.root, path)
         lb = self.label_dict[path.split(".")[0].split("_")[1]]
@@ -43,6 +47,7 @@ class TOUGH_C1:
                     "atom",
                     "atom_id atom_name residue_id residue_name x y z"
                 )
+        print(f"loading {path} ...")
         for pdb in tqdm(tar.getmembers()):
             if not pdb.isfile():
                 continue
@@ -76,6 +81,39 @@ class TOUGH_C1:
         
         return pdb_info
 
+    def _split_dataset(self, train_test_ratio):
+        random.seed(self.random_seed)
+        random.shuffle(self.control_ls)
+        random.shuffle(self.heme_ls)
+        random.shuffle(self.nucleotide_ls)
+        random.shuffle(self.steroid_ls)
+
+        train_ls = list()
+        test_ls = list()
+        self._list_extend(train_ls, test_ls, self.control_ls, train_test_ratio)
+        self._list_extend(train_ls, test_ls, self.heme_ls, train_test_ratio)
+        self._list_extend(
+            train_ls, test_ls, self.nucleotide_ls, train_test_ratio)
+        self._list_extend(train_ls, test_ls, self.steroid_ls, train_test_ratio)
+        random.shuffle(train_ls)
+        random.shuffle(test_ls)
+        return train_ls, test_ls
+
+    def _list_extend(self, train_ls, test_ls, target_ls, ratio):
+        splitor = int(len(target_ls)*ratio)
+        train_ls += target_ls[:splitor]
+        test_ls += target_ls[splitor:]
+
+class TOUGH_POINT(TOUGH_C1):
+
+    def __init__(self, random_seed=0, train_test_ratio=0.9, subset="train"):
+        super(TOUGH_POINT, self).__init__(random_seed, train_test_ratio)
+        self.subset = subset
+
+    def __next__(self):
+        pass
+
+
 
 if __name__ == "__main__":
     def print_first(d):
@@ -84,8 +122,16 @@ if __name__ == "__main__":
         print(d[key][0:10])
 
     t = TOUGH_C1()
-    print(f"control: {t.control_ls[0:2]}")
-    print(f"heme: {t.heme_ls[0:2]}")
-    print(f"nucleotide: {t.nucleotide_ls[0:2]}")
-    print(f"steroid: {t.steroid_ls[0:2]}")
-    print_first(t._load_tar("protein-control.tar.gz"))
+    # print(f"control: {t.control_ls[0:2]}")
+    # print(f"heme: {t.heme_ls[0:2]}")
+    # print(f"nucleotide: {t.nucleotide_ls[0:2]}")
+    # print(f"steroid: {t.steroid_ls[0:2]}")
+    # print_first(t._load_tar("protein-control.tar.gz"))
+    print(len(t.train_ls))
+    print(len(t.test_ls))
+    print(t.train_ls[:10])
+    print(t.test_ls[:10])
+    print(len(t.control_ls))
+    print(len(t.heme_ls))
+    print(len(t.nucleotide_ls))
+    print(len(t.steroid_ls))
