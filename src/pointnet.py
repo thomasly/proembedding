a85c836939defdc7d565b8dcdd7fb7e47721e498
Yang Liu <thomasliuy@gmail.com>
2019-08-27 13:11:57 -0400

    fix the pointnet

--- src/pointnet.py
+++ src/pointnet.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 from tensorflow.python.ops import array_ops
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense
+from tensorflow.keras.layers import BatchNormalization
 import numpy as np
 
 from data import TOUGH_POINT
@@ -18,18 +19,30 @@ class PointNet(Model):
     def __init__(self, channels, classes=4):
         super(PointNet, self).__init__()
         conv_kernel = (1, channels)
-        self.conv2d_32 = Conv2D(32, conv_kernel, activation="relu")
-        self.conv2d_64 = Conv2D(64, conv_kernel, activation="relu")
-        self.conv2d_128 = Conv2D(128, conv_kernel, activation="relu")
-        self.maxpool = MaxPool2D((128, 1))
-        self.dense_128 = Dense(128, activation="relu")
-        self.dense_64 = Dense(64, activation="relu")
+        self.conv2d_input = Conv2D(64, conv_kernel, activation="relu")
+        self.conv2d_64_64 = Conv2D(64, (1, 64), activation="relu")
+        self.conv2d_64_128 = Conv2D(128, (1, 64), activation="relu")
+        self.conv2d_128_1024 = Conv2D(1024, (1, 128), activation="relu")
+        self.maxpool = MaxPool2D((1, 1024))
+        self.dense_512 = Dense(512, activation="relu")
+        self.dense_256 = Dense(256, activation="relu")
         self.dense_output = Dense(classes, activation="softmax")
+        self.perm = (0, 1, 3, 2)
 
     def call(self, x):
-        x = self.conv2d_32(x)
-        x = self.maxpool(x)
-        x = self.dense_128(x)
+        x = self.conv2d_input(x) # batch_size x pc_len x 1 x 64
+        x = tf.transpose(x, self.perm) # batch_size x pc_len x 64 x 1
+        x = self.conv2d_64_64(x) # batch_size x pc_len x 1 x 64
+        x = tf.transpose(x, self.perm) # batch_size x pc_len x 64 x 1
+        x = self.conv2d_64_64(x) # batch_size x pc_len x 1 x 64
+        x = tf.transpose(x, self.perm) # batch_size x pc_len x 64 x 1
+        x = self.conv2d_64_128(x) # batch_size x pc_len x 1 x 128
+        x = tf.transpose(x, self.perm) # batch_size x pc_len x 128 x 1
+        x = self.conv2d_128_1024(x) # batch_size x pc_len x 1 x 1024
+        x = tf.transpose(x, self.perm) # batch_size x pc_len x 1024 x 1
+        x = self.maxpool(x) # batch_size x pc_len x 1 x 1
+        x = self.dense_512(x)
+        x = self.dense_256(x)
         x = self.dense_output(x)
         return x
 
@@ -60,6 +73,7 @@ def train():
         steps_per_epoch=tp.train_steps,
         epochs=10
     )
+    print(model.summary())
 
 if __name__ == "__main__":
     train()
