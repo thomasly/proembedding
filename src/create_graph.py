7c498cf935c15903252b6cd851fb3887cee438c2
Yang Liu <thomasliuy@gmail.com>
2019-09-05 11:40:24 -0400

    can save pocket graph with C-alpha only now

--- src/create_graph.py
+++ src/create_graph.py
@@ -1,10 +1,14 @@
 import os
+import sys
 import tarfile
+import pickle as pk
 from random import sample, randrange
 from collections import namedtuple
+import argparse
 
 import numpy as np
 import freesasa as fs
+from tqdm import tqdm
 
 class PDBtoPFH():
 
@@ -138,17 +142,40 @@ class PDBtoPFH():
                     self.all_ca, self.norms, self.ca_res, self.ca_solv)]
 
 
-# class Pocket2PFH(PDBtoPFH):
+class Pocket2PFH(PDBtoPFH):
 
-#     def __init__(self, file_path, residue_list, cutoff=7):
-#         super(Pocket2PFH, self).__init__(file_path, cutoff)
-#         self.residue_list = residue_list
-#         self.atom_to_int = self._load_atom_to_int() # implement this
+    def __init__(self, file_path, residue_list, cutoff=7):
+        super(Pocket2PFH, self).__init__(file_path, cutoff)
+        self.residue_list = residue_list
+
+    # get the coordinates of all C-alpha in the residue list
+    def _get_ca(self):
+        self.all_ca = []
+        self.ca_res = []
+        self.ca_solv = []
+        for atom in self.cl_data:
+            if atom.atom_name.upper() == "CA" and (
+                    atom.residue_id in self.residue_list):
+                try:
+                    self.ca_res.append(
+                        self.resi_to_int[atom.residue_name.upper()])
+                except KeyError:
+                    self.ca_res.append(self.resi_to_int["NAA"])
+                try:
+                    self.ca_solv.append(
+                        self.solvent_access.atomArea(atom.atom_id))
+                except Exception:
+                    self.ca_solv.append(0.0)
+                self.all_ca.append([atom.x, atom.y, atom.z])
+
+    def _get_c(self):
+        self.all_c = []
+        for atom in self.cl_data:
+            if atom.atom_name.upper() == "C" and (
+                    atom.residue_id in self.residue_list):
+                self.all_c.append([atom.x, atom.y, atom.z]) 
 
     
-
-    
-
 
                 
 class TOUGH_C1_Graph:
@@ -221,10 +248,9 @@ class TOUGH_C1_Graph:
         class_label = 0
         for subset in self.data_list:
             file_list = self.create_file_list(subset)
-            for pdb in file_list:
+            for pdb in tqdm(file_list):
                 if not pdb.name.endswith(".pdb"):
                     continue
-                print(pdb.path)
                 pdb_pfh = PDBtoPFH(pdb.path, cutoff=cutoff)
                 pdb_pfh.get_attributes()
                 a.write(self.get_adjacency_matrix(pdb_pfh, starting_number))
@@ -244,24 +270,95 @@ class TOUGH_C1_Graph:
 
 class TOUGH_C1_Pocket_Graph(TOUGH_C1_Graph):
 
-    def __init__(self, dataset, resi_list):
+    def __init__(self, dataset, pocket_type="lpc"):
         super(TOUGH_C1_Pocket_Graph, self).__init__(dataset)
-        self.resi_list = resi_list
-        
-        
+        self.pocket_type = pocket_type
+        self.resi_dict = self._create_resi_dict()
 
+    def _create_resi_dict(self):
+        file_list = [self.pocket_type+"-"+suffix+".residues" for suffix in [
+            "control", "nucleotide", "heme", "steroid"]]
+        resi_dict = dict()
+        for resi_f in file_list:
+            path = os.path.join(self.tough_dir, resi_f)
+            with open(path, "r") as f:
+                for line in f.readlines():
+                    if len(line) < 5:
+                        continue
+                    pdb_id = line[:5]
+                    resi_ids = list()
+                    for token in line.split()[4:]:
+                        resi_ids.append(int(token[1:]))
+                    resi_dict[pdb_id] = resi_ids
+        return resi_dict
+
+    def save_graph(self,
+                   file_prefix: str,
+                   cutoff=10):
+        """Convert pdb files to graph
+        """
+        save_path = os.path.join(
+            self.save_path, self.dataset+"_pocket", str(cutoff))
+        os.makedirs(save_path, exist_ok=True)
+
+        a = open(os.path.join(save_path, file_prefix+"_A.txt"), "w")
+        idc = open(
+            os.path.join(save_path, file_prefix+"_graph_indicator.txt"), "w")
+        g_label = open(
+            os.path.join(save_path, file_prefix+"_graph_labels.txt"), "w")
+        n_label = open(
+            os.path.join(save_path, file_prefix+"_node_labels.txt"), "w")
+
+        starting_number = 1
+        pdb_id = 1
+        class_label = 0
+        for subset in self.data_list:
+            file_list = self.create_file_list(subset)
+            for pdb in tqdm(file_list):
+                if not pdb.name.endswith(".pdb"):
+                    continue
+                residue_list = self.resi_dict[pdb.name.split(".")[0]]
+                pdb_pfh = Pocket2PFH(pdb.path, residue_list, cutoff=cutoff)
+                pdb_pfh.get_attributes()
+                a.write(self.get_adjacency_matrix(pdb_pfh, starting_number))
+                idc.write((str(pdb_id)+"\n")*len(pdb_pfh.all_ca))
+                g_label.write(str(class_label)+"\n")
+                for attr in pdb_pfh.ca_attributes:
+                    n_label.write(str(attr)[1:-1]+"\n")
+                starting_number += len(pdb_pfh.all_ca)
+                pdb_id += 1
+            class_label += 1
+
+        a.close()
+        idc.close()
+        g_label.close()
+        n_label.close()
 
 
 if __name__ == "__main__":
-    tcg = TOUGH_C1_Graph("tough-c1")
-    tcg.save_graph("TOUGH-C1")
-    tcg.save_graph("TOUGH-C1", 15)
-    tcg.save_graph("TOUGH-C1", 20)
-    tcg = TOUGH_C1_Graph("nucleotide")
-    tcg.save_graph("nucleotide")
-    tcg.save_graph("nucleotide", 15)
-    tcg.save_graph("nucleotide", 20)
-    tcg = TOUGH_C1_Graph("heme")
-    tcg.save_graph("heme")
-    tcg.save_graph("heme", 15)
-    tcg.save_graph("heme", 20)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pocket", action="store_true")
+    args = parser.parse_args(sys.argv[1:])
+    if args.pocket:
+        tcpg = TOUGH_C1_Pocket_Graph("tough-c1")
+        tcpg.save_graph("TOUGH-C1")
+        tcpg.save_graph("TOUGH-C1", 15)
+        tcpg = TOUGH_C1_Pocket_Graph("nucleotide")
+        tcpg.save_graph("nucleotide")
+        tcpg.save_graph("nucleotide", 15)
+        tcpg = TOUGH_C1_Pocket_Graph("heme")
+        tcpg.save_graph("heme")
+        tcpg.save_graph("heme", 15)
+    else:
+        tcg = TOUGH_C1_Graph("tough-c1")
+        tcg.save_graph("TOUGH-C1")
+        tcg.save_graph("TOUGH-C1", 15)
+        tcg.save_graph("TOUGH-C1", 20)
+        tcg = TOUGH_C1_Graph("nucleotide")
+        tcg.save_graph("nucleotide")
+        tcg.save_graph("nucleotide", 15)
+        tcg.save_graph("nucleotide", 20)
+        tcg = TOUGH_C1_Graph("heme")
+        tcg.save_graph("heme")
+        tcg.save_graph("heme", 15)
+        tcg.save_graph("heme", 20)
