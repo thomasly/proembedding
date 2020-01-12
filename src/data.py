fix docing dataset pointnet, add pca rotation function

src/data.py
@@ -8,6 +8,7 @@ import pickle as pk
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from sklearn.decomposition import PCA
 
 class BatchGenerator:
     """ Batch generator base class. Implement the _create_batch abstract method
@@ -92,7 +93,14 @@ class PointnetData(BatchGenerator):
         sample_counter = 0
         for i in batch_indices:
             sample_id = self.data_ls[i].id
-            label = self._one_hot(min(self.label_len-1, self.data_ls[i].label))
+            if self.label_len > 1:
+                label = self._one_hot(
+                    min(self.label_len-1, self.data_ls[i].label))
+            else:
+                if self.data_ls[i].label != 0:
+                    label = 1
+                else:
+                    label = 0
             data = self.dataset[sample_id]
             pointcloud = np.zeros(
                 (self.pointcloud_len, self.point_channels, 1),
@@ -155,7 +163,14 @@ class PointnetPocketData(PointnetData):
         sample_counter = 0
         for i in batch_indices:
             sample_id = self.data_ls[i].id
-            label = self._one_hot(min(self.label_len-1, self.data_ls[i].label))
+            if self.label_len > 1:
+                label = self._one_hot(
+                    min(self.label_len-1, self.data_ls[i].label))
+            else:
+                if self.data_ls[i].label != 0:
+                    label = 1
+                else:
+                    label = 0
             data = self.dataset[sample_id]
             pocket_residues = self.pockets[sample_id]
             pointcloud = np.zeros(
@@ -225,7 +240,7 @@ class TOUGH_C1:
             ret_ls.append(Protein(label=lb, id=line))
         return ret_ls
 
-    def _load_tar(self, path):
+    def _load_tar(self, path, pca_rotate=False):
         """ Load pdb files from the .tar.gz files
         """
         tar = tarfile.open(os.path.join(self.root, path))
@@ -279,9 +294,20 @@ class TOUGH_C1:
                     atom_id, atom_name, residue_id, residue_name, x, y, z)
                 atom_ls.append(atom)
             atom_df = pd.DataFrame(atom_ls, columns=Atom._fields)
-            atom_df.x = atom_df.x - atom_df.x.mean()
-            atom_df.y = atom_df.y - atom_df.y.mean()
-            atom_df.z = atom_df.z - atom_df.z.mean()
+
+            if pca_rotate:
+                atom_coor = atom_df[["x", "y", "z"]].to_numpy()
+                pca = PCA(n_components=3)
+                pca.fit(atom_coor)
+                atom_coor = pca.transform(atom_coor)
+                avgs = np.mean(atom_coor, axis=0)
+                atom_coor = atom_coor - avgs
+                atom_df[["x", "y", "z"]] = atom_coor
+            else:
+                atom_df.x = atom_df.x - atom_df.x.mean()
+                atom_df.y = atom_df.y - atom_df.y.mean()
+                atom_df.z = atom_df.z - atom_df.z.mean()
+
             pdb_info[name] = atom_df
         
         return pdb_info
@@ -325,10 +351,13 @@ class TOUGH_C1:
         train_ls += target_ls[:splitor]
         test_ls += target_ls[splitor:]
 
-    def _load_dataset(self):
+    def _load_dataset(self, pca_rotate=False):
         """ Load the whole dataset from .tar.gz files
         """
-        pointnet_data_path = os.path.join(self.bin, "tough_c1_data")
+        if pca_rotate:
+            pointnet_data_path = os.path.join(self.bin, "tough_c1_data_pca")
+        else:
+            pointnet_data_path = os.path.join(self.bin, "tough_c1_data")
         try:
             with open(pointnet_data_path, "rb") as f:
                 self.dataset = pk.load(f)
@@ -338,7 +367,7 @@ class TOUGH_C1:
                       "protein-heme.tar.gz",
                       "protein-nucleotide.tar.gz",
                       "protein-steroid.tar.gz"]:
-                self.dataset.update(self._load_tar(p))
+                self.dataset.update(self._load_tar(p, pca_rotate))
             with open(pointnet_data_path, "wb") as f:
                 pk.dump(self.dataset, f)
 
@@ -350,11 +379,13 @@ class TOUGH_Point(TOUGH_C1):
                  pointcloud_len=1024,
                  random_seed=0,
                  train_test_ratio=0.9,
+                 pca_rotate=False,
                  subset=None,
                  resi_name_channel=False,
                  label_len=4):
         super(TOUGH_Point, self).__init__(
             random_seed, train_test_ratio, subset)
+        self.pca_rotate = pca_rotate
         self.batch_size = batch_size
         self.pointcloud_len = pointcloud_len
         self.resi_name_channel = resi_name_channel
@@ -362,7 +393,7 @@ class TOUGH_Point(TOUGH_C1):
 
     def train(self):
         if self.dataset is None:
-            self._load_dataset()
+            self._load_dataset(self.pca_rotate)
         train_set = PointnetData(
             self.train_ls, self.dataset, self.batch_size,
             self.pointcloud_len, resi_name_channel=self.resi_name_channel,
@@ -373,7 +404,7 @@ class TOUGH_Point(TOUGH_C1):
 
     def test(self):
         if self.dataset is None:
-            self._load_dataset()
+            self._load_dataset(self.pca_rotate)
         test_set = PointnetData(
             self.test_ls, self.dataset, self.batch_size,
             self.pointcloud_len, if_shuffle=False,
@@ -408,6 +439,7 @@ class TOUGH_Point_Pocket(TOUGH_Point):
                  pointcloud_len=1024,
                  random_seed=0,
                  train_test_ratio=0.7,
+                 pca_rotate=False,
                  subset=None,
                  resi_name_channel=False,
                  atom_name_channel=False,
@@ -415,7 +447,7 @@ class TOUGH_Point_Pocket(TOUGH_Point):
                  pocket_type="lpc"):
         super(TOUGH_Point_Pocket, self).__init__(
             batch_size, pointcloud_len, random_seed, train_test_ratio,
-            subset, resi_name_channel, label_len)
+            pca_rotate, subset, resi_name_channel, label_len)
         self.atom_name_channel = atom_name_channel
         self.pocket_type = pocket_type
         self.pockets = None
@@ -455,7 +487,7 @@ class TOUGH_Point_Pocket(TOUGH_Point):
         if self.pockets is None:
             self._load_pockets()
         if self.dataset is None:
-            self._load_dataset()
+            self._load_dataset(self.pca_rotate)
         train_set = PointnetPocketData(
             self.train_ls, self.dataset, self.pockets, self.batch_size,
             self.pointcloud_len, atom_name_channel=self.atom_name_channel,
@@ -468,7 +500,7 @@ class TOUGH_Point_Pocket(TOUGH_Point):
         if self.pockets is None:
             self._load_pockets()
         if self.dataset is None:
-            self._load_dataset()
+            self._load_dataset(self.pca_rotate)
         test_set = PointnetPocketData(
             self.test_ls, self.dataset, self.pockets, self.batch_size,
             self.pointcloud_len, if_shuffle=False,

src/pointnet_docking_dataset.py
@@ -14,6 +14,7 @@ from tensorflow.keras.callbacks import TensorBoard, LearningRateScheduler
 from sklearn.model_selection import StratifiedKFold
 import numpy as np
 from sklearn.utils.class_weight import compute_sample_weight
+from sklearn.decomposition import PCA
 
 
 class PointNet(Model):
@@ -90,6 +91,9 @@ class PointnetArgParser(ArgumentParser):
                           help="Number of training epochs.")
         self.add_argument("-d", "--dropout-rate", default=0.1, type=float,
                           help="Drop rate of the dropout layers.")
+        self.add_argument("-p", "--pca", action="store_true",
+                          help="If to rotate the data by aligning to"
+                               " principle components.")
 
 
 def scheduler(epoch):
@@ -121,10 +125,18 @@ def find_longest_pointcloud(pointclouds):
     return max_len
 
 
-def load_data_kfold(k, input_path):
+def load_data_kfold(k, input_path, pca_rotate=False):
     with open(input_path, "rb") as f:
         data = pk.load(f)
     X_data = list(data.values())
+    #rotate points by aligning to principle components
+    if pca_rotate:
+        X_data = np.array(X_data)
+        for i, point_cloud in enumerate(X_data):
+            pca = PCA(n_components=3)
+            pca.fit(point_cloud[:, 0:3])
+            X_data[i][:, 0:3] = pca.transform(point_cloud[:, 0:3])
+        X_data = X_data.tolist()
     # find the longest pointcloud in the dataset
     max_len = find_longest_pointcloud(X_data)
     # pad all the point clouds with 0 to max len and convert to numpy array
@@ -138,9 +150,9 @@ def load_data_kfold(k, input_path):
     return folds, X_data, Y_data
 
  
-def train(in_path, out_path, k_fold, epochs, batch_size, drop_rate):
+def train(in_path, out_path, k_fold, epochs, batch_size, drop_rate, pca=False):
     # load and k-fold split the training dataset
-    folds, X_data, Y_data = load_data_kfold(k_fold, in_path)
+    folds, X_data, Y_data = load_data_kfold(k_fold, in_path, pca_rotate=pca)
     # print("folds: {}".format(folds))
     # print("x_data shape: {}".format(X_data.shape))
     
@@ -160,19 +172,23 @@ def train(in_path, out_path, k_fold, epochs, batch_size, drop_rate):
     auc_metric = tf.keras.metrics.AUC()
     precision_metric = tf.keras.metrics.Precision()
     recall_metric = tf.keras.metrics.Recall()
-    model.compile(
-        optimizer=optimizer,
-        loss="binary_crossentropy",
-        metrics=[
-            "binary_accuracy",
-            auc_metric,
-            precision_metric,
-            recall_metric
-        ]
-    )
     
     training_histories = list()
     for j, (train_idx, val_idx) in enumerate(folds):
+        tf.keras.backend.clear_session()
+        model = PointNet(channels=n_channels,
+            classes=classes,
+            drop_rate=drop_rate)
+        model.compile(
+            optimizer=optimizer,
+            loss="binary_crossentropy",
+            metrics=[
+                "binary_accuracy",
+                auc_metric,
+                precision_metric,
+                recall_metric
+            ]
+        )
         # training
         # print("\nFold:", j)
         X_train_cv = X_data[train_idx]
@@ -205,7 +221,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # train model
     histories = train(args.input_path, args.output_path, args.kfold,
-                      args.epochs, args.batch_size, args.dropout_rate)
+                      args.epochs, args.batch_size, args.dropout_rate,
+                      pca=args.pca)
 
     # analyze and save the training results
     val_accs = list()
@@ -240,21 +257,24 @@ if __name__ == "__main__":
     max_f1s = 2 * max_precisions * max_recalls / (max_precisions + max_recalls)
     f1_avg = np.mean(max_f1s)
     f1_std = np.std(max_f1s)
-    best_val = list(map(float, best_val))
-    avg = st.mean(best_val)
-    std = st.stdev(best_val)
+    # best_val = list(map(float, best_val))
+    # avg = st.mean(best_val)
+    # std = st.stdev(best_val)
 
     # write training result to file
     os.makedirs(args.output_path, exist_ok=True)
     log_prefix = os.path.basename(args.input_path).split(".")[0]
-    log_name = log_prefix+"_pointnet_training.log"
+    if args.pca:
+        log_name = log_prefix+"_pointnet_training.pca.log"
+    else:
+        log_name = log_prefix+"_pointnet_training.log"
     with open(os.path.join(args.output_path, log_name), "w") as f:
         print("Training dataset: {}".format(log_prefix), file=f)
         print("Batch size: {}".format(args.batch_size), file=f)
         print("Epochs: {}".format(args.epochs), file=f)
         print("{}-fold cross validation.".format(args.kfold), file=f)
         print("Input: {}".format(args.input_path), file=f)
-        print("Validation accuracy is {} +- {}".format(avg, std), file=f)
+        print("Validation accuracy is {} +- {}".format(acc_avg, acc_std), file=f)
         print("AUC ROC is {} +- {}".format(auc_avg, auc_std), file=f)
         print("Precision is {} +- {}".format(
             precision_avg, precision_std), file=f)